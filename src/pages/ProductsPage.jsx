bf83e421afc60117a1d6a9e525550011f6bd3342
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-09-18 18:14:29 -0400

    ProductList displayed

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
+const urlBase = "http://localhost:8080/api/producto";
 const ProductsPage = () => {
-  return <div>ProductsPage</div>;
+  const [productos, setProductos] = useState([]);
+  const [newF, setNewF] = useState(false);
+
+  useEffect(() => {
+    cargarProductos();
+  }, [newF]);
+
+  const cargarProductos = async () => {
+    const resultado = await axios.get(urlBase);
+    setProductos(resultado.data);
+  };
+
+  return (
+    <div className="big-container">
+      <h2>Lista de Productos</h2>
+
+      <button onClick={(evt) => console.log("Nuevo producto")}>Agregar</button>
+      <table>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th className="">Nombre</th>
+            <th className="">Nombre Extranjero</th>
+            <th className="">Cod Barra</th>
+            <th className="">Precio Lista</th>
+            <th className="">Peso</th>
+            <th className="">UM</th>
+            <th className="">Fabricante</th>
+            <th className="">Proveedor</th>
+            <th className="">Alternante</th>
+            <th className="">Grupo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {productos.map((producto, key) => {
+            console.log(producto);
+
+            return (
+              <tr key={key}>
+                <td>{producto.id}</td>
+                <td className="">{producto.nombre}</td>
+                <td className="">{producto.nombreExtranjero}</td>
+                <td className="">{producto.codBarra}</td>
+                <td className="">{producto.precioLista}</td>
+                <td className="">{producto.peso}</td>
+                <td className="">{producto.um}</td>
+                <td className="">{producto.fabricante?.nombre}</td>
+                <td className="">{producto.proveedor?.nombre}</td>
+                <td className="">{producto.alternante?.nombre}</td>
+                <td className="">{producto.grupoProducto?.nombre}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ProductsPage;
