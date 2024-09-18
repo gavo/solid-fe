44d8e809520311ba38b835240f54255e22e5a5af
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-09-18 16:40:15 -0400

    FrontPage ready to show

--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import task from "../assets/task.jpg";
 
 const FrontPage = () => {
   return (
-    <div>
+    <>
       <h1>Principios Solid</h1>
-    </div>
+      <p>
+        Desarrollar una aplicación Escritorio/Web para poder registrar los datos
+        maestro de un producto. Donde los campos mínimos son lo definido en la
+        clase base de ejemplo Aplicar programación en capas. Aplicar los
+        principios SOLID. Deben realizar una presentación donde mencione en que
+        parte utilizaron los principios SOLID y exponer.
+      </p>
+      <img alt="tarea" src={task} />
+    </>
   );
 };
 
