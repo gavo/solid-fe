8f0a537d2d4a7fcefb89a4f092d353ab579319c0
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-09-18 16:40:40 -0400

    footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>©{new Date().getFullYear()} - UAGRM - SOE - MDEIS - M7 - G3</footer>
+  );
+};
+
+export default Footer;
