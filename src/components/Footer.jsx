30ffdecc6b53b14ec902156fe12eb384f7997d1e
Gustavo Vargas Miranda <gvmgust@gmail.com>
2024-11-09 15:54:55 -0400

    prueba

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const Footer = () => {
 	return (
 		<footer>
-			©{new Date().getFullYear()} - UAGRM - SOE - MDEIS - M8 - G2
+			©{new Date().getFullYear()} - UAGRM - SOE - MDEIS - M8 - G2 -
+			Actualizado para revision
 		</footer>
 	);
 };
